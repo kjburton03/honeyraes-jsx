f061e66e8b7b268da5d86550df5dbb857dbf4ae7
kimberly burton <kj.burton03@gmail.com>
2024-10-22 09:21:38 -0400

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList"
 import { EmployeeList } from "./components/employees/EmployeeList"
 import { TicketList } from "./components/tickets/TicketList"
+import { Routes, Route, Outlet } from "react-router-dom"
+import { NavBar } from "./components/nav/NavBar"
 
 export const App = () => {
-  return <>
-    {/* <TicketList /> */}
-    <CustomerList />
-    <EmployeeList />
-  </>
+  return (
+    <Routes>
+      <Route path="/" 
+        element={
+          <>
+            <NavBar /> 
+            <Outlet />
+          </>
+        }
+      >
+      <Route path="tickets" element={ <TicketList />} />
+      <Route path="employees" element={ <EmployeeList />} />
+      <Route path="customers" element={ <CustomerList />} />
+      </Route>
+    </Routes>
+
+  )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom/client"
 import { App } from "./App.jsx"
 import "./index.css"
 
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container)
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+        <App /> 
+    </BrowserRouter>
+)
